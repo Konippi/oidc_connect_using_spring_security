f069f26a62c995bcb29e60cb7f00917faba1deeb
Konippi <konippi-114@outlook.jp>
2022-09-23 12:13:00 +0900

    completed google oidc

---
+++ src/main/java/com/example/controller/LoginController.java
@@ -0,0 +1,50 @@
+package com.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Controller
+public class LoginController {
+    /**
+     * root page
+     *
+     * @return success page or login page
+     */
+    @GetMapping("/")
+    public String index(@AuthenticationPrincipal final OidcUser loginUser) {
+        // logged in user
+        if (Objects.nonNull(loginUser)) {
+            return "redirect:/success";
+        }
+        return "redirect:/login";
+    }
+
+    /**
+     * success
+     *
+     * @return login success page
+     */
+    @GetMapping("/success")
+    public String success(Model model) {
+        model.addAttribute("message", "Login Success");
+        return "index";
+    }
+
+    /**
+     * failed
+     *
+     * @return login failed page
+     */
+    @GetMapping("/failed")
+    public String failed(Model model) {
+        model.addAttribute("message", "Login Failed");
+        return "index";
+    }
+}
